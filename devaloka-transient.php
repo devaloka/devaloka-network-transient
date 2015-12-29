610df268f9dfceaa9921256f32c47c7933d056c3
Whizark <devaloka@whizark.com>
2015-12-29 21:04:50 +0900

    chore: bump version to 0.1.2

--- devaloka-transient.php
+++ devaloka-transient.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Devaloka Transient
 Description: Provides Site-contextual Transient API
-Version: 0.1.1
+Version: 0.1.2
 Author: Whizark
 Author URI: http://whizark.com
 License: GPL-2.0+
